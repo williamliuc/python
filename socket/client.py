f050187e066f28502ad3394e1bb25a0f6f04b13b
SuperLiu <1476996891@qq.com>
2018-06-11 15:06:27 +0800

    Update client.py

--- socket/client.py
+++ socket/client.py
@@ -1,14 +1,25 @@
 #coding=utf-8
 from socket import *
+
+#创建socket
 tcpClientSocket=socket(AF_INET,SOCK_STREAM)
+
+#连接服务器
 serAddr=('172.16.0.30',7788)
 tcpClientSocket.connect(serAddr)#绑定服务器
+
 while True:
+        
+        #提示用户输入数据
         sendData=raw_input("send:")
         if len(sendData)>0:
                 tcpClientSocket.send(sendData)
         else:
                 break
+                
+        #接收对方发送过来的数据，最大接收1024个字节
         recvData=tcpClientSocket.recv(1024)
         print("recv:",recvData)
+        
+#关闭套接字
 tcpClientSocket.close()
